d62cf524eb7a7860c6d5e1b07a596efdf882ab2c
Jeremyn Lee <leej287@newschool.edu>
2015-09-10 23:23:52 -0400

    Added Clock

--- sketch.js
+++ sketch.js
@@ -1,18 +1,53 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(800, 560);
 }
 
 function draw() {
 	// clear the background
-	background(150, 0, 0);
+	background(119, 136, 153);
 
-	// set a fill color
-	fill(255, 255, 255);
+	//crown
+
+	fill(222, 184, 135);
+		noStroke();
+
+	triangle(403, 50, 381, 150, 423, 150);
+
+	triangle(353, 60, 381, 150, 323, 180);
+
+	triangle(453, 60, 481, 190, 423, 150);
+
+	ellipse(400, 245, 220, 220);
+
+	// set a fill color white
+	fill(255, 245, 225);
 
 	// set a stroke color
-	stroke(255, 0, 0);
+	stroke(255, 255, 255);
 
 	// draw a circle
-	rect(320, 180, 100, 100);
+		ellipse(400, 245, 200, 200);
+	noStroke();
+
+	fill(0, 0, 0);
+	// draw clockhand 1
+	rect(400, 155, 5, 95);
+
+	fill(0, 0, 0);
+	// draw clockhand 2
+	rect(400, 250, 95, 5);
+
+	fill(0, 0, 0);
+
+	triangle(403, 150, 391, 170, 413, 170);
+
+	triangle(490, 240, 490, 270, 505, 255);
+
+	ellipse(403, 345, 10, 10);
+
+	ellipse(303, 248, 10, 10);
+
+
+
 }
